703b94aa24b4446b13d3180988fc624784055e78
Bas Pennings <baspster@gmail.com>
2019-02-04 18:00:51 +0100

    Basic foundations done

--- src/Pixie.Core/Transform.cs
+++ src/Pixie.Core/Transform.cs
@@ -1,5 +1,7 @@
 namespace Pixie.Core
 {
+    using System;
+
     public static class Transform
     {
         public static Float4x4 Translate(float x, float y, float z) =>
@@ -14,6 +16,34 @@ namespace Pixie.Core
                 x, 0, 0, 0,
                 0, y, 0, 0,
                 0, 0, z, 0,
+                0, 0, 0, 1);
+
+        public static Float4x4 RotateX(float r) =>
+            new Float4x4(
+                1, 0, 0, 0,
+                0, (float)Math.Cos(r), -(float)Math.Sin(r), 0,
+                0, (float)Math.Sin(r), (float)Math.Cos(r), 0,
+                0, 0, 0, 1);
+
+        public static Float4x4 RotateY(float r) =>
+            new Float4x4(
+                (float)Math.Cos(r), 0, (float)Math.Sin(r), 0,
+                0, 1, 0, 0,
+                -(float)Math.Sin(r), 0, (float)Math.Cos(r), 0,
+                0, 0, 0, 1);
+
+        public static Float4x4 RotateZ(float r) =>
+            new Float4x4(
+                (float)Math.Cos(r), -(float)Math.Sin(r), 0, 0,
+                (float)Math.Sin(r), (float)Math.Cos(r), 0, 0,
+                0, 0, 1, 0,
+                0, 0, 0, 1);
+
+        public static Float4x4 Shear(float xy, float xz, float yx, float yz, float zx, float zy) =>
+            new Float4x4(
+                1, xy, xz, 0,
+                yx, 1, yz, 0,
+                zx, zy, 1, 0,
                 0, 0, 0, 1);
     }
 }
--- src/Pixie.Tests/TransformTests.cs
+++ src/Pixie.Tests/TransformTests.cs
@@ -72,13 +72,136 @@ namespace Pixie.Test
         [Fact]
         public void TestRotatePointAroundXAxis()
         {
-
+            var p = Float4.Point(0, 1, 0);
+            var halfQuarter = Transform.RotateX((float)Math.PI / 4);
+            var fullQuarter = Transform.RotateX((float)Math.PI / 2);
+            var halfExpected = Float4.Point(0, (float)Math.Sqrt(2) / 2, (float)Math.Sqrt(2) / 2);
+            var fullExpected = Float4.Point(0, 0, 1);
+            var comparer = new ApproxFloat4EqualityComparer(0.0000001f);
+            Assert.Equal(halfExpected, halfQuarter * p, comparer);
+            Assert.Equal(fullExpected, fullQuarter * p, comparer);
         }
 
         [Fact]
         public void TestInverseOfXRotationRotatesInOppositeDirection()
         {
+            var p = Float4.Point(0, 1, 0);
+            var halfQuarterInv = Transform.RotateX((float)Math.PI / 4).Inverse();
+            var expected = Float4.Point(0, (float)Math.Sqrt(2) / 2, -(float)Math.Sqrt(2) / 2);
+            var comparer = new ApproxFloat4EqualityComparer(0.0000001f);
+            Assert.Equal(expected, halfQuarterInv * p, comparer);
+        }
 
+        [Fact]
+        public void TestRotatePointAroundYAxis()
+        {
+            var p = Float4.Point(0, 0, 1);
+            var halfQuarter = Transform.RotateY((float)Math.PI / 4);
+            var fullQuarter = Transform.RotateY((float)Math.PI / 2);
+            var halfExpected = Float4.Point((float)Math.Sqrt(2) / 2, 0, (float)Math.Sqrt(2) / 2);
+            var fullExpected = Float4.Point(1, 0, 0);
+            var comparer = new ApproxFloat4EqualityComparer(0.0000001f);
+            Assert.Equal(halfExpected, halfQuarter * p, comparer);
+            Assert.Equal(fullExpected, fullQuarter * p, comparer);
+        }
+
+
+        [Fact]
+        public void TestRotatePointAroundZAxis()
+        {
+            var p = Float4.Point(0, 1, 0);
+            var halfQuarter = Transform.RotateZ((float)Math.PI / 4);
+            var fullQuarter = Transform.RotateZ((float)Math.PI / 2);
+            var halfExpected = Float4.Point(-(float)Math.Sqrt(2) / 2, (float)Math.Sqrt(2) / 2, 0);
+            var fullExpected = Float4.Point(-1, 0, 0);
+            var comparer = new ApproxFloat4EqualityComparer(0.0000001f);
+            Assert.Equal(halfExpected, halfQuarter * p, comparer);
+            Assert.Equal(fullExpected, fullQuarter * p, comparer);
+        }
+
+        [Fact]
+        public void TestSharingMovesXInProportionToY()
+        {
+            var t = Transform.Shear(1, 0, 0, 0, 0, 0);
+            var p = Float4.Point(2, 3, 4);
+            var expected = Float4.Point(5, 3, 4);
+            Assert.Equal(expected, t * p);
+        }
+
+        [Fact]
+        public void TestSharingMovesXInProportionToZ()
+        {
+            var t = Transform.Shear(0, 1, 0, 0, 0, 0);
+            var p = Float4.Point(2, 3, 4);
+            var expected = Float4.Point(6, 3, 4);
+            Assert.Equal(expected, t * p);
+        }
+
+        [Fact]
+        public void TestSharingMovesYInProportionToX()
+        {
+            var t = Transform.Shear(0, 0, 1, 0, 0, 0);
+            var p = Float4.Point(2, 3, 4);
+            var expected = Float4.Point(2, 5, 4);
+            Assert.Equal(expected, t * p);
+        }
+
+        [Fact]
+        public void TestSharingMovesYInProportionToZ()
+        {
+            var t = Transform.Shear(0, 0, 0, 1, 0, 0);
+            var p = Float4.Point(2, 3, 4);
+            var expected = Float4.Point(2, 7, 4);
+            Assert.Equal(expected, t * p);
+        }
+
+        [Fact]
+        public void TestSharingMovesZInProportionToX()
+        {
+            var t = Transform.Shear(0, 0, 0, 0, 1, 0);
+            var p = Float4.Point(2, 3, 4);
+            var expected = Float4.Point(2, 3, 6);
+            Assert.Equal(expected, t * p);
+        }
+
+        [Fact]
+        public void TestSharingMovesZInProportionToY()
+        {
+            var t = Transform.Shear(0, 0, 0, 0, 0, 1);
+            var p = Float4.Point(2, 3, 4);
+            var expected = Float4.Point(2, 3, 7);
+            Assert.Equal(expected, t * p);
+        }
+
+        [Fact]
+        public void TestIndividualTransformationsAppliedInSequence()
+        {
+            var a = Transform.RotateX((float)Math.PI / 2);
+            var b = Transform.Scale(5, 5, 5);
+            var c = Transform.Translate(10, 5, 7);
+            var p1 = Float4.Point(1, 0, 1);
+            var p2 = a * p1;
+            var p3 = b * p2;
+            var p4 = c * p3;
+            var comparer = new ApproxFloat4EqualityComparer(0.000001f);
+            Assert.Equal(Float4.Point(1, -1, 0), p2, comparer);
+            Assert.Equal(Float4.Point(5, -5, 0), p3, comparer);
+            Assert.Equal(Float4.Point(15, 0, 7), p4, comparer);
+        }
+
+        [Fact]
+        public void TestChainedTransformationsAppliedInReverseOrder()
+        {
+            var a = Transform.RotateX((float)Math.PI / 2);
+            var b = Transform.Scale(5, 5, 5);
+            var c = Transform.Translate(10, 5, 7);
+            var p = Float4.Point(1, 0, 1);
+            var t = c * b * a;
+            // Note that we can execute this with higher precision 
+            // than if we would apply the transformations in sequence like
+            // in the previous test case.
+            var comparer = new ApproxFloat4EqualityComparer(0.0000001f);
+            Assert.Equal(Float4.Point(15, 0, 7), t * p, comparer);
         }
     }
 }
