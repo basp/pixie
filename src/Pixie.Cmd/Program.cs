75695b366d6931c5be6182973a8ff27b216a58f2
Bas Pennings <baspster@gmail.com>
2019-02-04 22:36:42 +0100

    Almost ready to Phong

--- src/Pixie.Cmd/Program.cs
+++ src/Pixie.Cmd/Program.cs
@@ -10,8 +10,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine(Banner);
-            var canvas = CanvasExamples.Example1();
-            canvas.SavePpm(@"d:\temp\frotz.ppm");
-        }
+
+            var sphere = new Sphere();
+
+            sphere.Material = 
+                new Material
+                {
+                    Color = new Color(1, 0.2f, 1),
+                };
+
+            var lightPosition = Float4.Point(-10, 10, -10);
+            var lightColor = new Color(1, 1, 1);
+            var light = new PointLight(lightPosition, lightColor);
+       }
     }
 }
