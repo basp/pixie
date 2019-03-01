fc43a56176488f89572861be9944017386a7bb53
Bas Pennings <baspster@gmail.com>
2019-03-01 15:52:00 +0100

    Formatting

--- src/Pixie.Core/Double4x4.cs
+++ src/Pixie.Core/Double4x4.cs
@@ -26,10 +26,11 @@ namespace Pixie.Core
             };
         }
 
-        public Double4x4(double m00, double m01, double m02, double m03,
-                         double m10, double m11, double m12, double m13,
-                         double m20, double m21, double m22, double m23,
-                         double m30, double m31, double m32, double m33)
+        public Double4x4(
+            double m00, double m01, double m02, double m03,
+            double m10, double m11, double m12, double m13,
+            double m20, double m21, double m22, double m23,
+            double m30, double m31, double m32, double m33)
         {
             this.data = new[]
             {
